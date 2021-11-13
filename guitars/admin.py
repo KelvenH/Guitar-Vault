d10392f4f041e54901f7c8c996d11f4bd3f37423
KelvenH <kelvenhughes@me.com>
2021-11-13 13:45:59 +0000

    Customised Admin views

--- guitars/admin.py
+++ guitars/admin.py
@@ -1,5 +1,28 @@
 from django.contrib import admin
 from .models import Guitar, Category
 
-admin.site.register(Guitar)
-admin.site.register(Category)
+
+class GuitarAdmin(admin.ModelAdmin):
+    list_display = (
+        'brand',
+        'guitar_model',
+        'category',
+        'tier',
+        'status',
+        'owner',
+        'condition',
+        'rating_condition',
+        'rating_overall',
+    )
+
+    ordering = ('brand', 'guitar_model')
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Guitar, GuitarAdmin)
+admin.site.register(Category, CategoryAdmin)
