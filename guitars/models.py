fa57294afee0f5090cd4528133b5892f36c80f67
KelvenH <kelvenhughes@me.com>
2021-11-13 13:32:49 +0000

    Added customisation to guitar model to introduce choices and enhanced data validation rules

--- guitars/models.py
+++ guitars/models.py
@@ -19,7 +19,16 @@ class Category(models.Model):
 
 
 class Guitar(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    ELECTRIC = '1'
+    BASS = '2'
+    ACOUSTIC = '3'
+
+    CATEGORY_CHOICES = [
+        (ELECTRIC, 'Electric'),
+        (BASS, 'Bass'),
+        (ACOUSTIC, 'Acoustic'),
+    ]
+    category = models.ForeignKey('Category', choices=CATEGORY_CHOICES, default=ELECTRIC, on_delete=models.PROTECT) # PROTECT prevents deletion of the primary key (categories)
     brand = models.CharField(max_length=25)
     guitar_model = models.CharField(max_length=25)
     tier = models.CharField(max_length=25, null=True, blank=True)
@@ -28,12 +37,33 @@ class Guitar(models.Model):
     handed = models.CharField(max_length=25, null=True, blank=True)
     no_strings = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
     approx_age_years = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
-    condition = models.CharField(max_length=25, null=True, blank=True)
+
+    """ Choices to create dropdown selection for users """
+    EXCELLENT = 'Excellent'
+    GOOD = 'Good'
+    COSMETIC = 'Cosmetic'
+    DEFECTIVE = 'Defective'
+    CONDITION_CHOICES = [
+        (EXCELLENT, 'Excellent'),
+        (GOOD, 'Good'),
+        (COSMETIC, 'Well Used (displays cosmetic signs of use only)'),
+        (DEFECTIVE, 'Some form of damage / wear which affects performance. (No guitars can be added to the vault if defective. Existing guitars rated by users as defective will be marked as "unavailable" until inspected by Guitar Vault'),
+        ]
+    condition = models.CharField(max_length=25, choices=CONDITION_CHOICES, default=GOOD)
     construction = models.CharField(max_length=25, null=True, blank=True)
     body_wood = models.CharField(max_length=25, null=True, blank=True)
     body_top = models.CharField(max_length=25, null=True, blank=True)
     tuners = models.CharField(max_length=25, null=True, blank=True)
-    frets = models.DecimalField(max_digits=2, decimal_places=0, null=True, blank=True)
+    
+    FRETS_20 = '20'
+    FRETS_22 = '22'
+    FRETS_24 = '24'
+    FRETS_CHOICES = [
+        (FRETS_20, '20'),
+        (FRETS_22, '22'),
+        (FRETS_24, '24'),
+        ]
+    frets = models.DecimalField(max_digits=2, decimal_places=0, choices=FRETS_CHOICES, default=FRETS_22)
     scale_length = models.CharField(max_length=25, null=True, blank=True)
     neck_wood = models.CharField(max_length=25, null=True, blank=True)
     neck_profile = models.CharField(max_length=25, null=True, blank=True)
