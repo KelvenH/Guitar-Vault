cfa90b7d134dbef6f1c42ffc7c3229ffd97fb412
KelvenH <kelvenhughes@me.com>
2021-11-19 15:45:31 +0000

    Added search functionality

--- guitars/views.py
+++ guitars/views.py
@@ -1,13 +1,27 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Guitar
 
 def all_guitars(request):
     """ A view to show all guitars, including sorting and search queries """
 
     guitars = Guitar.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Provide a guitar brand or model name for me to search for")
+                return redirect(reverse('guitars'))
+            
+            queries = Q(brand__icontains=query) | Q(guitar_model__icontains=query)
+            guitars = guitars.filter(queries)
 
     context = {
         'guitars': guitars,
+        'search_term': query,
     }
 
     return render(request, 'guitars/guitars.html', context)
