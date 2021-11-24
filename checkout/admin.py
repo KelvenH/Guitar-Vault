e0c1e8ff3b0580aa5e4b84a55852ac45fde02a84
KelvenH <kelvenhughes@me.com>
2021-11-24 17:57:18 +0000

    Created checkout models and admin view

--- checkout/admin.py
+++ checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('order_number', 'date', 'subscription_price',)
+
+    fields = ('order_number', 'date', 'subscription_plan', 'subscription_price', 'full_name', 'email', 'phone_number', 'street_address1', 'street_address2', 'town_or_city', 'postcode', 'county', 'country',)
+
+    list_display = ('order_number', 'date', 'full_name', 'subscription_plan', 'subscription_price',)
+
+    ordering = ('full_name',)
+
+admin.site.register(Order, OrderAdmin)
