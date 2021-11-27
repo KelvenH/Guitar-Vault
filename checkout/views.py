e95555fa550b36f6c0da5da890ef45ab531496d7
KelvenH <kelvenhughes@me.com>
2021-11-27 13:52:01 +0000

    Added basic checkout functionality. O/s BUG: Whilst payment successfully received by Stripe, there is no form reset or  indication to user

--- checkout/views.py
+++ checkout/views.py
@@ -30,14 +30,17 @@ def checkout(request):
         currency=settings.STRIPE_CURRENCY,
     )
 
-    print(intent)
-
     order_form = OrderForm()
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing.\
+            Did you forget to set it in your environment?')
+
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51K09ihIfuGDQGaewEbM0Wd0V3LX4bM3mtQrEisAhQFHvjrBMsL6zkk4EEg49kOermXCi7ZSHQ8wvv0AG8uX0HUHI00J6V68xZs',
-        'client_secret': 'test client secret',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
