477447d1997e37b6b9804fe460555c12756c8850
KelvenH <kelvenhughes@me.com>
2021-11-27 17:45:37 +0000

    Initiated function to add order upon payment

--- checkout/views.py
+++ checkout/views.py
@@ -14,23 +14,48 @@ def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
     stripe_secret_key = settings.STRIPE_SECRET_KEY
 
-    bag = request.session.get('bag', {})
-    if not bag:
-        messages.error(request, "There's nothing in our bag, take a look at our subscription plans to proceed")
-        return redirect(reverse('subscriptions'))
+    if request.method == 'POST':
+        bag = request.session.get('bag',{})
+        form_data = {
+            'subscription_plan': request.POST['subscription_plan'],
+            'full_name': request.POST['full_name'],
+            'email': request.POST['email'],
+            'phone_number': request.POST['phone_number'],
+            'street_address1': request.POST['street_address1'],
+            'street_address2': request.POST['street_address2'],
+            'town_or_city': request.POST['town_or_city'],
+            'county': request.POST['county'],
+            'postcode': request.POST['postcode'],
+            'country': request.POST['country'],
+        }
+        order_form = OrderForm(form_data)
+        if order_form.is_valid():
+            order_form.save()
+            for item_id, item_data in bag.items():
+                try:
+                    subscription = Subscription.objects.get(id=item_id)
+                    if isinstance(item_data, int):
+                        
 
-    # get total cost from bag
-    current_bag = bag_contents(request)
-    total = current_bag['bag_total']
-    # convert total to integer for Stripe
-    stripe_total = round(total * 100)
-    stripe.api_key = stripe_secret_key
-    intent = stripe.PaymentIntent.create(
-        amount=stripe_total,
-        currency=settings.STRIPE_CURRENCY,
-    )
 
-    order_form = OrderForm()
+    else:
+        bag = request.session.get('bag', {})
+        if not bag:
+            messages.error(request, "There's nothing in our bag, take a look at our subscription plans to proceed")
+            return redirect(reverse('subscriptions'))
+
+        # get total cost from bag
+        current_bag = bag_contents(request)
+        total = current_bag['bag_total']
+        # convert total to integer for Stripe
+        stripe_total = round(total * 100)
+        stripe.api_key = stripe_secret_key
+        intent = stripe.PaymentIntent.create(
+            amount=stripe_total,
+            currency=settings.STRIPE_CURRENCY,
+        )
+
+        order_form = OrderForm()
 
     if not stripe_public_key:
         messages.warning(request, 'Stripe public key is missing.\
