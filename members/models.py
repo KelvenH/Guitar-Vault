21fd682e29bdc2e77002c124722a3a41ab714cc1
KelvenH <kelvenhughes@me.com>
2021-11-29 20:34:18 +0000

    Added styling to allauth templates, started view to display users profile

--- members/models.py
+++ members/models.py
@@ -10,8 +10,6 @@ class MemberProfile(models.Model):
     Member profile page holding personal information
     """
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    default_full_name = models.CharField(max_length=50, null=True, blank=True)
-    default_email = models.EmailField(max_length=254, null=True, blank=True)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
     default_street_address1 = models.CharField(max_length=80, null=True, blank=True)
     default_street_address2 = models.CharField(max_length=80, null=True, blank=True)
--- members/views.py
+++ members/views.py
@@ -1,11 +1,14 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import MemberProfile
 
 def profile(request):
     """
     Display user's profile
     """
+    profile = get_object_or_404(MemberProfile, user=request.user)
     template = 'members/profile.html'
-    context = {}
+    context = {
+        'profile': profile,
+    }
 
     return render(request, template, context)
