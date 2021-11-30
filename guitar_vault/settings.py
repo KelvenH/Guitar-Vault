c5f4328779625381e0d2def29aea0301008b4f9c
KelvenH <kelvenhughes@me.com>
2021-11-30 11:58:59 +0000

    Removed default SECRET_KEY and mapped to variable saved in Heroku config var. Also set debug in development only

--- guitar_vault/settings.py
+++ guitar_vault/settings.py
@@ -10,10 +10,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b#hp&$c%k^!xwuna#bm1otcvg29+-*a%72-rr*@^h1r5**ts2&'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['guitar-vault.herokuapp.com', 'localhost']
 
