ffb5fdf962526c0bdd60556b8530196691ae41f2
KelvenH <kelvenhughes@me.com>
2021-11-30 13:43:41 +0000

    Added stripe wh secret to settings

--- guitar_vault/settings.py
+++ guitar_vault/settings.py
@@ -199,7 +199,7 @@ if 'USE_AWS' in os.environ:
 STRIPE_CURRENCY = 'gbp'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
-
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 
 # Default primary key field type
