789da409c738e6082690ea3c63dd1c81f4d24e29
KelvenH <kelvenhughes@me.com>
2021-12-01 20:56:21 +0000

    Added missing JS semi-colons

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,13 @@
 document.addEventListener("DOMContentLoaded", function() {
 /*-- cycle through members feedback (index.html for non-logged-in visitors) --*/
     const feedbackA = "Having access to guitars I would not otherwise get a chance to play gives pure enjoyment!";
-    const memberA = "Paul Wright"
+    const memberA = "Paul Wright";
     const feedbackB = "I was a bit sceptical at first, but all instruments have been in fantastic condition. A great service for the guitarist who wants an affordable way to swap their rig!";
-    const memberB = "James Hendrix"
+    const memberB = "James Hendrix";
     const feedbackC = "I love getting to choose what guitar to try out next and the collection and delivery has been first class!";
-    const memberC = "Suzie B"
+    const memberC = "Suzie B";
     const feedbackD = "There's such a great choice of amazing guitars, and I can see that new models are being added regularly to broaden the choices!";
-    const memberD = "Matt Bellamy"
+    const memberD = "Matt Bellamy";
     const feedbackList = [feedbackA, feedbackB, feedbackC, feedbackD];
     const memberList = [memberA, memberB, memberC, memberD];
     const membersFeedback = document.querySelector("#members-feedback");
