5571ea2b64dfb878c07a93f9a8db76f2513e0a73
KelvenH <kelvenhughes@me.com>
2022-01-25 21:13:26 +0000

    Re-working of lost commit which applies functionality to site admin orders

--- members/admin.py
+++ members/admin.py
@@ -1,18 +1,1 @@
 from django.contrib import admin
-from .models import MembersPlans
-
-
-class MembersPlansAdmin(admin.ModelAdmin):
-    list_display = (
-        'member_profile',
-        'subscription_plan',
-        'subscription_price',
-        'start_date',
-        'active',
-        'canx_date',
-    )
-
-    ordering = ('member_profile', 'active')
-
-
-admin.site.register(MembersPlans, MembersPlansAdmin)
--- site_admin/models.py
+++ site_admin/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+"""
+class Accounts(models.Model):
+    
+    Logs history of members subscription plans current and past
+    
+    # match user to member profile
+    member_profile = models.ForeignKey(MemberProfile,
+                                       on_delete=models.SET_NULL,
+                                       null=True, blank=True)
+
+    # get list of subscription plans purchased
+    subscription_plan = models.ForeignKey(Subscription, null=False,
+                                          blank=False,
+                                          on_delete=models.PROTECT)
+    # capture price paid
+    subscription_price = models.DecimalField(max_digits=4, decimal_places=2,
+                                             null=False, default=0)
+
+    # date plan purchased
+    start_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+
+    # is plan active or cancelled
+    active = models.BooleanField(default=False)
+
+    # date if plan cancelled 
+    canx_date = models.DateTimeField(auto_now=False, auto_now_add=False)
+    """
